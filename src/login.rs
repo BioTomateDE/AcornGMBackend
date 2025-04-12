a2b9cf95e7311ef1d322b51faba671acaefd7c8d
biotomatede <latus.kati@gmail.com>
2025-04-12 21:15:12 +0200

    more register and login fixes

--- src/login.rs
+++ src/login.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::sync::{Arc, LazyLock};
 use serde::Deserialize;
-use reqwest::Client;
+use reqwest::{Client, StatusCode};
 use rocket::http::Status;
 use crate::accounts::AcornAccount;
 use rocket::response::status;
@@ -32,8 +32,21 @@ struct TokenResponse {
 struct DiscordUserInfo {
     pub id: String,
     pub username: String,
-    pub email: Option<String>,
-    pub verified: bool,
+    pub avatar: String,
+    pub discriminator: String,
+    pub public_flags: u64,
+    pub flags: u64,
+    pub banner: Option<String>,
+    pub accent_color: u32,
+    pub global_name: String,
+    pub avatar_decoration_data: Option<String>,
+    pub collectibles: Option<String>,
+    pub banner_color: String,
+    pub clan: Option<String>,
+    pub primary_guild: Option<String>,
+    pub mfa_enabled: bool,
+    pub locale: String,
+    pub premium_type: u32,
 }
 
 
@@ -46,10 +59,10 @@ fn respond_err(status: Status, error_message: &str) -> RespType {
         })),
     )
 }
-fn respond_ok(json: Value) -> RespType {
+fn respond_ok(json_response: Value) -> RespType {
     status::Custom(
         Status::Ok,
-        Json(json!(json)),
+        Json(json!(json_response)),
     )
 }
 
@@ -58,7 +71,7 @@ const DISCORD_API_BASE_URL: &'static str = "https://discord.com/api/v10";
 const REDIRECT_URI: &'static str = "https://acorngm.onrender.com/discord_auth_page.html";
 const DISCORD_APP_CLIENT_ID: &'static str = "1360325253766578479";
 
-async fn get_access_token(discord_app_client_secret: &str, params: HashMap<&str, &str>) -> Result<TokenResponse, String> {
+async fn get_access_token(discord_app_client_secret: &str, params: HashMap<&str, &str>) -> Result<TokenResponse, (Status, String)> {
     let client: Client = Client::new();
     let res = client
         .post(format!("{}/oauth2/token", DISCORD_API_BASE_URL))
@@ -66,18 +79,23 @@ async fn get_access_token(discord_app_client_secret: &str, params: HashMap<&str,
         .form(&params)
         .send()
         .await
-        .map_err(|e| format!("Request failed: {e}"))?;
+        .map_err(|e| (Status::InternalServerError, format!("Request failed: {e}")))?;
 
     let status = res.status();
     if !status.is_success() {
-        error!("Error while getting access token from discord - {}: {}", status, res.text().await.unwrap_or_else(|_| "<invalid response text>".to_string()));
-        return Err(format!("Could not refresh discord token because discord returned a failure response: {}", status));
+        // check if code is invalid; because if it is, the error is the client's fault
+        let text: String = res.text().await.unwrap_or_else(|_| "<invalid response text>".to_string());
+        if text.contains("Invalid \\\"code\\\" in request") {
+            return Err((Status::Unauthorized, "Could not refresh discord token because the provided code is invalid".to_string()));
+        }
+        error!("Error while getting access token from discord - {}: {}", status, text);
+        return Err((Status::InternalServerError, format!("Could not refresh discord token because discord returned a failure response: {}", status)));
     }
 
-    res.json::<TokenResponse>().await.map_err(|error| format!("Failed to parse JSON while refreshing discord token: {error}"))
+    res.json::<TokenResponse>().await.map_err(|error| (Status::InternalServerError, format!("Failed to parse JSON while refreshing discord token: {error}")))
 }
 
-async fn exchange_code(discord_app_client_secret: &str, code: &str) -> Result<TokenResponse, String> {
+async fn exchange_code(discord_app_client_secret: &str, code: &str) -> Result<TokenResponse, (Status, String)> {
     let mut params = HashMap::new();
     params.insert("grant_type", "authorization_code");
     params.insert("code", &code);
@@ -85,7 +103,7 @@ async fn exchange_code(discord_app_client_secret: &str, code: &str) -> Result<To
     get_access_token(discord_app_client_secret, params).await
 }
 
-async fn refresh_token(discord_app_client_secret: &str, refresh_token: &str) -> Result<TokenResponse, String> {
+async fn refresh_token(discord_app_client_secret: &str, refresh_token: &str) -> Result<TokenResponse, (Status, String)> {
     let mut params = HashMap::new();
     params.insert("grant_type", "refresh_token");
     params.insert("refresh_token", refresh_token);
@@ -125,11 +143,11 @@ impl DiscordHandler {
         }
     }
 
-    pub async fn handle_get_discord_auth(&self, code: &str) -> status::Custom<Json<Value>> {
+    pub async fn api_get_discord_auth(&self, code: &str) -> status::Custom<Json<Value>> {
         // Get access/refresh tokens from OAuth2 code
         let token_response: TokenResponse = match exchange_code(&self.discord_app_client_secret, code).await {
             Ok(token_response) => token_response,
-            Err(error) => return status::Custom(Status::InternalServerError, Json(json!({
+            Err((status, error)) => return status::Custom(status, Json(json!({
                 "error": format!("Error while getting discord access token: {error}"),
             })))
         };
@@ -142,21 +160,23 @@ impl DiscordHandler {
 
         // check if account already exists; if it does, return acorn access token
         for account in self.accounts.clone().read().await.iter() {
-            if account.discord_id != user_info.id { continue }
-            return respond_ok(json!({
-                "register": true,
-            }));
+            if account.discord_id == user_info.id {
+                return respond_ok(json!({
+                    "register": false,
+                }));
+            }
         }
 
         // account does not exist; let client register
         respond_ok(json!({
-            "register": false,
-            "discordAccessToken": token_response.access_token,
+            "register": true,
+            "discordRefreshToken": token_response.refresh_token,
             "discordUserId": user_info.id,
+            "discordUsername": user_info.username,
         }))
     }
 
-    pub async fn handle_post_register(&self, register_data: Json<RegisterRequest>) -> status::Custom<Json<Value>> {
+    pub async fn api_post_register(&self, register_data: Json<RegisterRequest>) -> status::Custom<Json<Value>> {
         static USERNAME_REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"^[a-zA-Z0-9._-]+$").expect("Could not load username verification pattern"));
 
         if !USERNAME_REGEX.is_match(&register_data.username) {
@@ -167,7 +187,7 @@ impl DiscordHandler {
         // validate refresh token and discord user id
         let token_response: TokenResponse = match refresh_token(&self.discord_app_client_secret, &register_data.discord_refresh_token).await {
             Ok(token) => token,
-            Err(e) => return respond_err(Status::Unauthorized, &e),
+            Err((status, e)) => return respond_err(status, &e),
         };
 
         let user_info: DiscordUserInfo = match get_user_info(&token_response.access_token).await {
@@ -178,9 +198,6 @@ impl DiscordHandler {
         if user_info.id != register_data.discord_user_id {
             return respond_err(Status::Unauthorized, "The provided discord user ID does not belong to the provided discord access token!");
         }
-        if !user_info.verified {
-            return respond_err(Status::Forbidden, "Please verify your discord account!");
-        }
 
         // check if there is already an AcornGM account connected to this discord user
         for account in self.accounts.clone().read().await.iter() {
@@ -188,8 +205,6 @@ impl DiscordHandler {
                 return respond_err(Status::Conflict, "There is already an AcornGM account connected to this discord account!");
             }
         }
-
-        // TODO more checks maybe
 
         // success; add to account list
         let account = AcornAccount {
@@ -209,11 +224,11 @@ impl DiscordHandler {
 }
 #[get("/discord_auth?<discord_code>")]
 pub async fn api_get_discord_auth(handler: &State<DiscordHandler>, discord_code: &str) -> status::Custom<Json<Value>> {
-    handler.handle_get_discord_auth(discord_code).await
+    handler.api_get_discord_auth(discord_code).await
 }
 #[post("/register", data = "<register_data>")]
 pub async fn api_post_register(handler: &State<DiscordHandler>, register_data: Json<RegisterRequest>) -> status::Custom<Json<Value>> {
-    handler.handle_post_register(register_data).await
+    handler.api_post_register(register_data).await
 }
 
 #[derive(Deserialize)]
